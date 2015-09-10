223d21967dd845a5f30288adaafdbc3b4f91a4f2
Andrew <recrut.01@mail.ru>
2015-09-10 13:16:53 +0300

    Hello World!

---
+++ Task1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 10.09.2015.
+ */
+public class Task1 {
+    public static void main(String[] args) { //метод main- точка входа в программу/
+        System.out.println("Hello world!");
+    }
+}
