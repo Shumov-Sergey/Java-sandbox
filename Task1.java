228b8cb5efc9a216d240141533847f2ef7c20bd4
marker91 <bb26106@gmail.com>
2015-09-18 20:43:10 +0600

    неправильное условие "<=" приводило к некорректному результату = 234168, исправлено

--- Task1.java
+++ Task1.java
@@ -4,7 +4,7 @@
 public class Task1 {
     public static void main(String[] args) {
         int x = 0;
-        for (int i = 0; i<=1000; i++) {
+        for (int i = 0; i<1000; i++) {
             if (i%3==0 | i%5==0){
                 x += i;
             }
