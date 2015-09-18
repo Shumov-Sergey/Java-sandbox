68243befcbee380dc8e9588a7f8207d5313c61f8
marker91 <bb26106@gmail.com>
2015-09-18 18:16:06 +0600

    Синтаксис языка. Первое задание.

---
+++ Task1.java
@@ -0,0 +1,14 @@
+/**
+ * Created by marker on 17/09/2015.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i = 0; i<=1000; i++) {
+            if (i%3==0 | i%5==0){
+                x += i;
+            }
+        }
+        System.out.print(x);
+    }
+}
