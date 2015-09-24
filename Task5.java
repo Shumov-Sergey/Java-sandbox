4d20142e5a2dc4b225e7bb0d8adab5dbfc1b47d7
ixxaem <miggy@mail.ru>
2015-09-24 10:54:02 +0300

    Поправил

--- Task5.java
+++ Task5.java
@@ -7,14 +7,15 @@ import java.util.Collections;
  * Created by miroshnichenko on 23.09.2015.
  */
 public class Task5 {
-    /*
-    a1 есть, a3 есть, куда делось a2?
-    Из таких названий ничего не понятно
-    */
-    public ArrayList<Integer> a1 = new ArrayList();
-    public ArrayList<Integer> a3 = new ArrayList();
 
 
+
+    public ArrayList<Integer> globalDels = new ArrayList();
+
+
+
+
+    // Проверяем является ли полученное число простым
     public boolean isPrime(long num)
     {
         double nn =  java.lang.Math.round(java.lang.Math.sqrt(num));
@@ -33,64 +34,73 @@ public class Task5 {
     }
 
 
-    //Аналогичный комментарий, как к задаче 3
+
+    // получение делителей исходного числа
     public void getDelis( int num)
     {
-        int a = 0;
-        while (a<num)
+
+
+        ArrayList<Integer> localDels = new ArrayList();
+
+        // составляем список делителей числа
+        int tmp = 0;  // временная переменная.
+        while (tmp<num)
         {
-            a = a+1;
-            if((num%a==0)&&(isPrime(a)))
+            tmp = tmp+1;
+            if((num%tmp==0)&&(isPrime(tmp)))
             {
-             //   System.out.println(a);
-                a1.add(a);
-                num = num/a;
-                a = 1;
+                localDels.add(tmp);
+                num = num/tmp;
+                tmp = 1;
             }
+
+
         }
 
 
-       int i = 0;
-        //Как это работает?
-        for (int j = 0; j < a1.size(); j++) {
+        /*
+            Последовательно перебираем все значения массива простых  делителей
+            Сравниваем с глобальным массивом.
+            Если локальный массив не входит в глобальный массив - добавляем в глобальный массив недостающие значения
+         */
 
-            if(Collections.frequency(a3,a1.get(j))<Collections.frequency(a1,a1.get(j)))
+        for (int j = 0; j < localDels.size(); j++) {
+
+            if(Collections.frequency(globalDels,localDels.get(j))<Collections.frequency(localDels,localDels.get(j)))
             {
-              int b = a1.get(j);// (int) Math.pow(a1.get(j),(Collections.frequency(a1,a1.get(j))-Collections.frequency(a3,a1.get(j))));
-                a3.add(b);
-                //можно так a3.add(a1.get(j));
+                globalDels.add(localDels.get(j));
             }
 
         }
-        a1.clear();
+        localDels.clear();
     }
 
 
-    /*
-        К сожалению, совершенно не понял, как это работает.
-        Утонул в a,a1,a3,m,n,nn,s.......
-        В первых двух задачах мне больше нравились именования
-    */
-    public static void main(String[] args)
+
+    public  void main(String[] args)
+
     {
 
-        Task5 m = new Task5();
+        // при написании алгоритма использовалось http://math-prosto.ru/?page=pages/find_nod_and_nok/find_nok.php .
         long timestart=System.currentTimeMillis();
-        for (int i = 2; i < 20; i++) {
-            m.getDelis(i);
+        // список делителей для которых ищем значение
+
+        for (int i = 0; i < args.length; i++) {
+            getDelis(Integer.valueOf(args[i]));
         }
 
 
         int s = 1;
-        for (int i = 0; i < m.a3.size(); i++) {
+        // последовательно перемножаем все делители
+        for (int i = 0; i < this.globalDels.size(); i++) {
 
-            s = s * m.a3.get(i);
+            s = s * this.globalDels.get(i);
         }
 
         long timeend=System.currentTimeMillis();
         System.out.println("time:" + (timeend - timestart));
 
-        System.out.println("s:" + s);
+        System.out.println("Наибольшее общее кратное:" + s);
 
 
     }
