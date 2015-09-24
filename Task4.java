5f3a59f7a1c39cc5883b699c158be9a764b592b6
ixxaem <miggy@mail.ru>
2015-09-24 10:37:27 +0300

    Поправил

--- Task4.java
+++ Task4.java
@@ -10,24 +10,13 @@ import static java.util.Collections.sort;
  */
 public class Task4 {
 
-    public ArrayList<Integer> aaa = new ArrayList();
 
-    public boolean isPol(int a)
-    {
+// проверка  - является ли число полиндромом. Разделяем число на разряды и переворачиваем
+public boolean isPol(int a)
+{
 
-    int razr = (int) java.lang.Math.floor( java.lang.Math.log10(a)  )+1;
-/*
-    int delta1 =(int) java.lang.Math.floor(razr/2);
-    double del1 = (razr%2==0)? delta1:delta1+1;
-    double del2 = delta1;
-    del1 = java.lang.Math.pow(10,del1);
-    del2 = java.lang.Math.pow(10,del2);
-    double leftP = java.lang.Math.floor(a/del1);
-    double rightP = a%del2;
-
-    System.out.println(leftP+" "+rightP);
-  */
-   int m = a;
+    int razr = (int) java.lang.Math.floor( java.lang.Math.log10(a)  )+1; // количество разрядов
+    int m = a;
     int l = 0;
 
     for (int i = 0; i < razr; i++) {
@@ -38,33 +27,42 @@ public class Task4 {
     }
 
 
-    return a==l;
+    return a==l;  // равно ли исходное число перевернутому
 }
-//Аналогичный комментарий, как к задаче 3
+
    public void getListPol()
    {
 
-       int pol = 0;
+       ArrayList<Integer> proizv = new ArrayList();  // тут хранятся произведения множителей
+
        long timestart=System.currentTimeMillis();
+
+       // так как в условии только 3-х значные числа
        for (int i = 1000; i > 99 ; i--) {
 
            for (int j = i; j > 99 ; j--) {
 
                if(isPol(i * j))
                {
-                   aaa.add(i*j);
+                   proizv.add(i*j);
                }
 
            }
        }
-       sort(aaa);
+       sort(proizv);  // сортируем значения по возрастанию
        long timeend=System.currentTimeMillis();
        System.out.println("time:" + (timeend - timestart));
 
-       System.out.println("Самый большой: "+ aaa.get(aaa.size()-1));
+       System.out.println("Самый большой: "+ proizv.get(proizv.size()-1));  
    }
 
 
-}
 
-//А где main()?
+    public void main()
+
+    {
+        this.getListPol();
+    }
+
+
+    }
